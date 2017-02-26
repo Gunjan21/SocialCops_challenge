559c2eb8defdb70ffed8b6ddf0a004a67d25941c
Gunjan <dewangunjan6012@gmail.com>
2017-02-26 13:46:50 +0530

    Some changes

--- assignment_1.py
+++ assignment_1.py
@@ -11,23 +11,51 @@ import pandas as pd
 sh1 = pd.read_excel("Tab_Villages_Mapping, Krishna District, 25 July 2015.xlsx")
 sh2 = pd.read_excel("Tab_Villages_Mapping, Krishna District, 25 July 2015.xlsx", sheetname = 1)
 
-#convert all dates to datetime format
-sh2["Survey End Date"] = pd.to_datetime(sh2["Survey End Date"])
-sh2["Survey Start Date"] = pd.to_datetime(sh2["Survey Start Date"])
 
-##checking for equal tab numbers in both dataframes
-##Copying the village names if the Survey Dates also match.
+## making sure the Dates are in datetime format
+sh2.dtypes
+'''
+AC Name                      object
+Mandal Name                  object
+Village Name                 object
+Tab No                        int64
+Survey Start Date    datetime64[ns]
+Survey End Date      datetime64[ns]
+dtype: object
+'''
+
+sh1.dtypes
+'''
+Response No              int64
+Tab No                   int64
+Survey Date     datetime64[ns]
+AC Name                float64
+Mandal Name            float64
+Village Name           float64
+dtype: object
+'''
+
+#checking for equal tab numbers in both dataframes
+#copying the village names if the Survey Dates also match.
+#There are some survey dates overlapping for the same Tab No.
+#In such cases, all the village names are being predicted.
+#Either one of them is true.
 for i in range(len(sh1["Tab No"])):
+    village_list = []
     for j in range(len(sh2["Tab No"])):
         if sh1["Tab No"][i] == sh2["Tab No"][j]:
-            ##python magic ! (never expected this to work with dates) :)
             if sh2["Survey Start Date"][j] <= sh1["Survey Date"][i] <= sh2["Survey End Date"][j]:
-                sh1["Village Name"][i] = sh2["Village Name"][j]
+                    if sh2['Village Name'][j] not in village_list:
+                        village_list.append(sh2["Village Name"][j])
+    sh1['Village Name'][i] = village_list
+
 
 #writing the predicted village names to a file.
-sh1.to_excel("predicted_villages.xls")
+sh1.to_excel("predicted_villages.xlsx")
 
-'''                    
-18218 villages have been predicted out of 21531
-i.e, 84.613% of the villages have been predicted
+'''
+19655 villages have been predicted out of 21531
+i.e. 91.291% of the villages have been predicted
+Out of these, 2351 have 2 village predictions 
+and 132 have 3 village predictions.
 '''
